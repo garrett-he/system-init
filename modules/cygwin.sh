5a1c4a0afea84f4135a501ac72eff524e4e642f1
Garrett HE <garrett.he@hotmail.com>
2022-11-20 11:43:50 +0800

    Add cygwin module: cygwin_apt-cyg

--- modules/cygwin.sh
+++ modules/cygwin.sh
@@ -5,3 +5,14 @@ module_cygwin_skel() {
 
 	cp "$SYSINIT_ROOT"/config/cygwin/.minttyrc ~
 }
+
+module_cygwin_apt-cyg() {
+    sysinit_read 'cygwin mirror' SYSINIT_MIRROR_CYGWIN
+
+    git clone https://github.com/transcode-open/apt-cyg.git /usr/local/apt-cyg
+
+    ln -s /usr/local/apt-cyg/apt-cyg /usr/local/bin
+
+    apt-cyg cache /var/cache/cygwin
+    apt-cyg mirror "$SYSINIT_MIRROR_CYGWIN"
+}
