de7839fb9383c370964fe2f7108d4ea8f8c8129a
Garrett HE <garrett.he@hotmail.com>
2022-11-22 11:53:31 +0800

    Add gitee mirrors for module: zsh

--- modules/common.sh
+++ modules/common.sh
@@ -9,14 +9,39 @@ module_dotfiles() {
     ./install.sh
 }
 
-module_ohmyzsh() {
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+module_zsh() {
+    # Install ohmyzsh
+    if [[ -z $SYSINIT_MIRROR_ZSH_OHMYZSH_GIT_REMOTE ]]; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    else
+        git clone "$SYSINIT_MIRROR_ZSH_OHMYZSH_GIT_REMOTE" /tmp/ohmyzsh
+        cd /tmp/ohmyzsh/tools
+        REMOTE=$SYSINIT_MIRROR_ZSH_OHMYZSH_GIT_REMOTE sh install.sh --unattended
 
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+        rm -rf /tmp/ohmyzsh
+    fi
+
+    # Install powerlevel10k
+    if [[ -z $SYSINIT_MIRROR_ZSH_POWERLEVEL10K_GIT_REMOTE ]]; then
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+    else
+        git clone --depth=1 $SYSINIT_MIRROR_ZSH_POWERLEVEL10K_GIT_REMOTE $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+    fi
+
     sysinit_sed 's#ZSH_THEME="robbyrussell"#ZSH_THEME="powerlevel10k/powerlevel10k"#g' ~/.zshrc
 
-    git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+    # Install zsh-autosuggestions and zsh-syntax-highlighting
+    if [[ -z $SYSINIT_MIRROR_ZSH_ZSH_AUTOSUGGESTIONS_GIT_REMOTE ]]; then
+        git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    else
+        git clone $SYSINIT_MIRROR_ZSH_ZSH_AUTOSUGGESTIONS_GIT_REMOTE $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    fi
+
+    if [[ -z $SYSINIT_MIRROR_ZSH_ZSH_SYNTAX_HIGHLIGHTING_GIT_REMOTE ]]; then
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+    else
+        git clone $SYSINIT_MIRROR_ZSH_ZSH_SYNTAX_HIGHLIGHTING_GIT_REMOTE $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    fi
 
     sysinit_sed 's#plugins=(git)#plugins=(zsh-autosuggestions zsh-syntax-highlighting)#g' ~/.zshrc
 }
