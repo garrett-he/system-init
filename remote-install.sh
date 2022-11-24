6f46c742d6764510dd2213f96e7a91e2fee220f8
Garrett HE <garrett.he@hotmail.com>
2022-11-24 10:41:13 +0800

    Keep system-init after remote install

--- remote-install.sh
+++ remote-install.sh
@@ -2,6 +2,7 @@
 
 SYSINIT_GIT_REMOTE=https://github.com/garrett-he/system-init.git
 
-git clone $SYSINIT_GIT_REMOTE /tmp/system-init
-bash /tmp/system-init/install.sh < /dev/tty
-rm -rf /tmp/system-init
+mkdir -p ~/.local/share
+
+git clone $SYSINIT_GIT_REMOTE ~/.local/share/system-init
+bash ~/.local/share/system-init/install.sh < /dev/tty
