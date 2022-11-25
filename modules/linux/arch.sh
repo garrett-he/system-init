05a754805bdc013018935bce331d7dc7b86af047
Garrett HE <garrett.he@hotmail.com>
2022-11-25 11:37:35 +0800

    Add --noconfirm option for linux package install

--- modules/linux/arch.sh
+++ modules/linux/arch.sh
@@ -1,20 +1,20 @@
 module_linux_python() {
-    sudo pacman -S python python-pip
+    sudo pacman -S --noconfirm python python-pip
 
     sysinit_append_shell_profile 'export PATH="$HOME/.local/bin:$PATH"'
 }
 
 module_linux_lua() {
-    sudo pacman -S lua luarocks
+    sudo pacman -S --noconfirm lua luarocks
     sysinit_append_shell_profile 'export PATH="$HOME/.luarocks/bin:$PATH"'
 }
 
 module_linux_php() {
-    sudo pacman -S php
+    sudo pacman -S --noconfirm php composer
 }
 
 module_linux_clash() {
-    sudo pacman -S clash
+    sudo pacman -S --noconfirm clash
 
     systemctl enable --user clash
     systemctl start --user clash
