cdc963ec87be40f97b75d09c231a6a3a020b7879
Garrett HE <garrett.he@outlook.com>
2023-09-05 11:50:42 +0800

    Add archlinux module: linux_lua

--- modules/linux/arch.sh
+++ modules/linux/arch.sh
@@ -5,3 +5,11 @@ module_linux_python() {
     sysinit::append_profiles '# linux_python'
     sysinit::append_profiles 'export PATH="$HOME/.local/bin:$PATH"'
 }
+
+module_linux_lua() {
+    sudo pacman -S --noconfirm lua luarocks
+
+    sysinit::append_profiles
+    sysinit::append_profiles '# linux_lua'
+    sysinit::append_profiles 'export PATH="$HOME/.luarocks/bin:$PATH"'
+}
