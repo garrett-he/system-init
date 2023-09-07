49f819b9d8af753b133a3072013d40ab01e4842a
Garrett HE <garrett.he@outlook.com>
2023-09-07 10:59:20 +0800

    Add remote installer script file

---
+++ remote-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [[ -z $SYSINIT_GIT_REMOTE ]]; then
+    SYSINIT_GIT_REMOTE=https://github.com/garrett-he/system-init.git
+fi
+
+git clone $SYSINIT_GIT_REMOTE /tmp/system-init
+/tmp/system-init/install.sh "$SYSINIT_FLAGS" < /dev/tty
+
+rm -rf /tmp/system-init
