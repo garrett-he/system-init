dfe8427b275f9ea9a01ff7567f6827d7a49105f3
Garrett HE <garrett.he@outlook.com>
2024-08-15 11:13:56 +0800

    Add common module: zsh

--- modules/common.sh
+++ modules/common.sh
@@ -6,3 +6,18 @@ module_dotfiles() {
     git submodule update
     ./install.sh
 }
+
+module_zsh() {
+    # Install ohmyzsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+    # Install powerlevel10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+    file::sed 's#ZSH_THEME="robbyrussell"#ZSH_THEME="powerlevel10k/powerlevel10k"#g' ~/.zshrc
+
+    # Install zsh-autosuggestions and zsh-syntax-highlighting
+    sysinit::git_clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    sysinit::git_clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+    file::sed 's#plugins=(git)#plugins=(zsh-autosuggestions zsh-syntax-highlighting)#g' ~/.zshrc
+}
