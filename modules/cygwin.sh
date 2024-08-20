db1a469261fff621847e09519a110adc1516a115
Garrett HE <garrett.he@outlook.com>
2024-08-20 11:42:30 +0800

    Add cygwin module: cygwin_apt-cyg

--- modules/cygwin.sh
+++ modules/cygwin.sh
@@ -5,3 +5,16 @@ module_cygwin_skel() {
 
 	cp "$SYSINIT_ROOT"/modules/cygwin/.minttyrc ~
 }
+
+module_cygwin_apt-cyg() {
+    if [[ -z "${SYSINIT_MIRROR_CYGWIN-}" ]]; then
+        utils::read 'cygwin mirror' SYSINIT_MIRROR_CYGWIN
+    fi
+
+    sysinit::git_clone https://github.com/transcode-open/apt-cyg.git /usr/local/apt-cyg SYSINIT_MIRROR_APT_CYG_GIT_REMOTE
+
+    ln -s /usr/local/apt-cyg/apt-cyg /usr/local/bin
+
+    apt-cyg cache /var/cache/cygwin
+    apt-cyg mirror "$SYSINIT_MIRROR_CYGWIN"
+}
